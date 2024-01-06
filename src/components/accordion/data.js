69164b2e70c73fb9a3f038714253002b5c0e3368
srgntpepper <chrismdavis2009@gmail.com>
2024-01-06 14:11:08 -0800

    This is just a demo of an accordion component with dummy data

--- src/components/accordion/data.js
+++ src/components/accordion/data.js
@@ -1,27 +1,27 @@
 const data = [
   {
     id: "1",
+    head: "Purus ut faucibus pulvinar elementum integer enim neque volutpat.",
     paragraph:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. In nisl nisi scelerisque eu ultrices vitae auctor eu augue. In hac habitasse platea dictumst vestibulum rhoncus est.",
-    more: "Purus ut faucibus pulvinar elementum integer enim neque volutpat.",
   },
   {
     id: "2",
+    head: "Ornare aenean euismod elementum nisi quis eleifend.",
     paragraph:
       "Velit aliquet sagittis id consectetur purus ut. Egestas sed tempus urna et pharetra pharetra. Sagittis eu volutpat odio facilisis mauris sit. Vestibulum sed arcu non odio euismod lacinia at quis risus. In mollis nunc sed id semper risus in. Volutpat blandit aliquam etiam erat. Egestas sed tempus urna et pharetra pharetra massa.",
-    more: "Ornare aenean euismod elementum nisi quis eleifend.",
   },
   {
     id: "3",
+    head: "Vel turpis nunc eget lorem dolor sed viverra ipsum.",
     paragraph:
       "At imperdiet dui accumsan sit amet nulla facilisi. Ornare massa eget egestas purus viverra accumsan in nisl. Dictum varius duis at consectetur lorem. Ultricies tristique nulla aliquet enim. ",
-    more: "Vel turpis nunc eget lorem dolor sed viverra ipsum.",
   },
   {
     id: "4",
+    head: "Nibh nisl condimentum id venenatis a condimentum vitae sapien.",
     paragraph:
       "Nisl pretium fusce id velit ut tortor pretium. Congue quisque egestas diam in arcu cursus. Felis eget nunc lobortis mattis aliquam faucibus purus in. Sit amet est placerat in egestas erat. Vitae congue mauris rhoncus aenean vel elit scelerisque mauris.",
-    more: "Nibh nisl condimentum id venenatis a condimentum vitae sapien.",
   },
 ];
 
--- src/components/accordion/index.jsx
+++ src/components/accordion/index.jsx
@@ -1,3 +1,69 @@
+import { useState } from "react";
+
+import data from "./data";
+import "./styles.css";
+//single selection
+
+//multiple selection
+
 export default function Accordion() {
-  return <div>Accordion</div>;
+  const [selected, setSelected] = useState(null);
+  const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+  const [multiple, setMultiple] = useState([]);
+
+  function handleSingleSelection(getCurrentId) {
+    setSelected(getCurrentId === selected ? null : getCurrentId);
+  }
+
+  function handleMutiSelection(getCurrentId) {
+    setMultiple((prevMultiple) => {
+      const updatedSelection = [...prevMultiple];
+      const indexOfId = updatedSelection.indexOf(getCurrentId);
+
+      if (indexOfId === -1) updatedSelection.push(getCurrentId);
+      else updatedSelection.splice(indexOfId, 1);
+
+      return updatedSelection;
+    });
+  }
+
+  return (
+    <div className="wrapper">
+      <button
+        onClick={() => {
+          setEnableMultiSelection((prevState) => !prevState);
+          setSelected(null);
+          setMultiple([]);
+        }}
+        className="multiselection"
+      >
+        {enableMultiSelection ? `Disable` : `Enable`} Multi-Selection
+      </button>
+      <div className="accordion">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item" key={dataItem.id}>
+              <div
+                onClick={
+                  enableMultiSelection
+                    ? () => handleMutiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
+                className="head"
+              >
+                <h3>{dataItem.head}</h3>
+                <span>+</span>
+              </div>
+              {selected === dataItem.id ||
+              multiple.indexOf(dataItem.id) !== -1 ? (
+                <div className="paragraph">{dataItem.paragraph}</div>
+              ) : null}
+            </div>
+          ))
+        ) : (
+          <div>No data found </div>
+        )}
+      </div>
+    </div>
+  );
 }
